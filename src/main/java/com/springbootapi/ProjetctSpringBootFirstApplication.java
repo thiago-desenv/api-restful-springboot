572178d0f29f7a8343f7493a8767f94ab3471971
Thiago Xavier <thiago_osim@hotmail.com>
2019-12-01 03:05:29 -0300

    Versão inicial do projeto api restful

---
+++ src/main/java/com/springbootapi/ProjetctSpringBootFirstApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetctSpringBootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetctSpringBootFirstApplication.class, args);
+	}
+
+}
